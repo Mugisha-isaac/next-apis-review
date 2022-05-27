df5122e2e53de463df4d3c74124b4a636832eb5d
Mugisha-isaac <mugishaisaac2001@gmail.com>
2022-05-27 12:59:40 +0200

    wip(001-helloworld.sql) creating migrations

---
+++ apis-review/migrations/001-helloworld.sql
@@ -0,0 +1,19 @@
+--UP
+   
+   CREATE TABLE Person(
+       id INTERGER  PRIMARY KEY AUTOINCREMENT,
+       name TEXT,
+       email TEXT
+   );
+
+   CREATE TABLE Vehicle(
+       id INTERGER  PRIMARY KEY AUTOINCREMENT,
+       brand TEXT,
+       model TEXT,
+       ownerId INTERGER FOREIGN KEY REFERENCES Person(id)
+   );
+
+--DOWN
+
+DROP TABLE Person;
+DROP TABLE Vehicle;
