f9235c08c7a5e16043269a9439225cb287308d77
Mugisha-isaac <mugishaisaac2001@gmail.com>
2022-05-27 21:57:44 +0200

    wip(people.ts) integrating with backend

--- apis-review/pages/api/people.ts
+++ apis-review/pages/api/people.ts
@@ -1,6 +1,5 @@
 import {NextApiRequest,NextApiResponse} from 'next';
 
-
 export default async function getAllPeople(req:NextApiRequest,res:NextApiResponse){
     res.send({message:'hello world'});
 }
--- apis-review/utils/server/model/person.ts
+++ apis-review/utils/server/model/person.ts
@@ -1,3 +1,20 @@
 import  mongoose from 'mongoose';
 
-// const Schema = mongoose
+const Schema = mongoose.Schema;
+
+const personSchema = new Schema({
+    name:{
+        type:String,
+        required:true,
+        max:12,
+        min:3
+    },
+    email:{
+        type:String,
+        required:true,
+        max:30,
+        min:5
+    }
+})
+
+module.exports = mongoose.model('Person', personSchema);
