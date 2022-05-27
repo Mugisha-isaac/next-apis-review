bf5be0c970ebf903b99159989dfa1b50c37020b2
Mugisha-isaac <mugishaisaac2001@gmail.com>
2022-05-27 20:28:35 +0200

    wip(server/) server configs

--- apis-review/pages/api/people.ts
+++ apis-review/pages/api/people.ts
@@ -1,5 +1,6 @@
-import {NextApiRequest,NextApiResponse} from 'next'
+import {NextApiRequest,NextApiResponse} from 'next';
 
-export default function getAllPeople(req:NextApiRequest,res:NextApiResponse){
-    res.send([{name:'p1'},{name:'p2'}, {name:'p3'}]);
+
+export default async function getAllPeople(req:NextApiRequest,res:NextApiResponse){
+    res.send({message:'hello world'});
 }
